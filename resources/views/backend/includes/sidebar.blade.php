10860a5377b85157cbb71488ca1a7ad57b137cdd
Ravi Bastola <bastola.ravi@gmail.com>
2017-04-05 06:33:52 +0545

    minor_changes

--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -25,8 +25,8 @@
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu">
             <li class="header">MAIN NAVIGATION</li>
-            <li class="treeview">
-                <a href="#">
+            <li class="{{Request::is('admin/dashboard*') ? 'active' : ''}}">
+                <a href="{{route('admin.dashboard.index')}}">
                     <i class="fa fa-dashboard"></i> <span>Dashboard</span>
                     <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
